eed76a823cec4df3d27eda71b7ff5a93828ec0ae
Charly <charly.silva.v@gmail.com>
2025-12-18 09:57:33 -0300

    fix: add cancel button and timeout for group-initiated donation flow (v3.12.2)

--- utils/helpers.py
+++ utils/helpers.py
@@ -458,7 +458,7 @@ def validate_facebook_credentials(config_obj) -> tuple[bool, str]:
     return True, ""
 
 
-CURRENT_VERSION = "3.12.1"
+CURRENT_VERSION = "3.12.2"
 
 
 def get_current_version() -> str:
