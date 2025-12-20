045c9dc56d9fc538c8d55c4ae445768a6245b8f8
Charly <charly.silva.v@gmail.com>
2025-12-20 02:01:50 -0300

    chore: bump CURRENT_VERSION to v4.2.0

--- utils/helpers.py
+++ utils/helpers.py
@@ -458,7 +458,7 @@ def validate_facebook_credentials(config_obj) -> tuple[bool, str]:
     return True, ""
 
 
-CURRENT_VERSION = "v4.1.0"
+CURRENT_VERSION = "v4.2.0"
 
 
 def get_current_version() -> str:
